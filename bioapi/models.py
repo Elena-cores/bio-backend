3e264e65f1bff0572a412dc271de7895b072b34c
Elena Cores <elenayunxin@gmail.com>
2024-12-05 20:03:28 +0100

    configuracion app, models, alguna url, etc

--- bioapi/models.py
+++ bioapi/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Photo(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    likes = models.IntegerField(default = 0)
+    
+    def __str__(self):
+        return f"{self.title} - {self.location} - {self.likes}"
+    
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    pub_date = models.DateTimeField(default = datetime.now)
+    photos = models.ManyToManyField(Photo)
+
+    def __str__(self):
+        return f"{self.title} - {self.pub_date}"
